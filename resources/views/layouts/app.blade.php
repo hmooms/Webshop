e97b01f4dc09bf8d057b5cfac65539c5c1f23ce9
99046763 <99046763@mydavinci.nl>
2019-05-10 21:23:14 +0900

    Revert "ik heb een database gemaakt"
    This reverts commit f0f421610c2b32d09ff549fe6bb8dd261326f1e6.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,12 +20,11 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name') }}
+                    {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
