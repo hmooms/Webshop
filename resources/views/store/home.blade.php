472b7c2de0e8c7a190842478efdb12ee1d6e230c
99046763 <99046763@mydavinci.nl>
2019-06-10 06:27:38 +0900

    not much but something

--- resources/views/store/home.blade.php
+++ resources/views/store/home.blade.php
@@ -10,11 +10,11 @@
 
 @foreach($products as $product)
 
-<div class="card text-white bg-secondary m-2" onclick="location.href = '{{ route('store.show', ['product_id' => $product->id]) }}'" style="text-align:center;max-width: 20rem;">
-  <div class="card-header">Header</div>
+<div class="card text-white bg-secondary m-2" onclick="location.href = '{{ route('store.show', ['product_id' => $product->id]) }}'" style="text-align:center;width: 20rem;">
+  <div class="card-header">{{$product->name}}</div>
   <div class="card-body">
-    <h4 class="card-title">Secondary card title</h4>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h4 class="card-title">€{{$product->price}}</h4>
+    <p class="card-text">{{$product->category->description}}</p>
   </div>
 </div>
 
