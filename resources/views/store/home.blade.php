2474a524883ed0dc336b9ed182e0afe970f18d12
99046763 <99046763@mydavinci.nl>
2019-06-15 18:58:25 +0200

    fixed a wrong redirect and removed some shit

--- resources/views/store/home.blade.php
+++ resources/views/store/home.blade.php
@@ -13,7 +13,7 @@
 <div class="card text-white bg-secondary m-2" onclick="location.href = '{{ route('store.show', ['product_id' => $product->id]) }}'" style="text-align:center;width: 20rem;">
   <div class="card-header">{{$product->name}}</div>
   <div class="card-body">
-    <h4 class="card-title">€{{$product->price}}</h4>
+    <h4 class="card-title">€{{$product->price}},99</h4>
     <p class="card-text">{{$product->category->description}}</p>
   </div>
 </div>
